91af15ca6da9c320d7ed42e1dc0dbf6678b1055d
Martin Aleksandrov <73018624+santush87@users.noreply.github.com>
2022-06-12 16:03:33 +0200

    Class Box Data Validation to 100%

---
+++ src/Encapsulation_Exercises/ClassBox_01/Main.java
@@ -0,0 +1,7 @@
+package Encapsulation_Exercises.ClassBox_01;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/Encapsulation_Exercises/Main.java
@@ -0,0 +1,7 @@
+package Encapsulation_Exercises;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
